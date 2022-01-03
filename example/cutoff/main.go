8d6e0b7983204429413848ff600bf0c0b4d5f987
Will Charczuk <wcharczuk@users.noreply.github.com>
2022-01-03 13:46:03 -0800

    updates

--- example/cutoff/main.go
+++ example/cutoff/main.go
@@ -8,18 +8,24 @@ import (
 	incr "github.com/wcharczuk/go-incremental"
 )
 
+// epsilon returns a function that returns true
+// if the absolute difference of two values is greater
+// than a given delta.
 func epsilon(delta float64) func(float64, float64) bool {
 	return func(v0, v1 float64) bool {
 		return math.Abs(v1-v0) > delta
 	}
 }
 
+// addConst returns a map fn that adds a constant value
+// to a given input
 func addConst(v float64) func(float64) float64 {
 	return func(v0 float64) float64 {
 		return v0 + v
 	}
 }
 
+// add is a map2 fn that adds two values and returns the result
 func add(v0, v1 float64) float64 {
 	return v0 + v1
 }
