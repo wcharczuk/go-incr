db3e14492ff805467dac2382ebe5115432b5d38b
Will Charczuk <wcharczuk@users.noreply.github.com>
2022-05-11 22:05:23 -0700

    includes bench data

--- bench_test.go
+++ bench_test.go
@@ -28,6 +28,26 @@ func Benchmark_Stabilize_withPreInitialize_8192(b *testing.B) {
 	benchmarkSize(8192, b)
 }
 
+func Benchmark_ParallelStabilize_withPreInitialize_512(b *testing.B) {
+	benchmarkParallelSize(512, b)
+}
+
+func Benchmark_ParallelStabilize_withPreInitialize_1024(b *testing.B) {
+	benchmarkParallelSize(1024, b)
+}
+
+func Benchmark_ParallelStabilize_withPreInitialize_2048(b *testing.B) {
+	benchmarkParallelSize(2048, b)
+}
+
+func Benchmark_ParallelStabilize_withPreInitialize_4096(b *testing.B) {
+	benchmarkParallelSize(4096, b)
+}
+
+func Benchmark_ParallelStabilize_withPreInitialize_8192(b *testing.B) {
+	benchmarkParallelSize(8192, b)
+}
+
 func benchmarkSize(size int, b *testing.B) {
 	rand.Seed(time.Now().Unix())
 
@@ -57,6 +77,38 @@ func benchmarkSize(size int, b *testing.B) {
 			SetStale(nodes[rand.Intn(size)])
 		}
 		_ = Stabilize(testContext(), gs)
+	}
+}
+
+func benchmarkParallelSize(size int, b *testing.B) {
+	rand.Seed(time.Now().Unix())
+
+	nodes := make([]Incr[string], size)
+	for x := 0; x < size; x++ {
+		nodes[x] = Var(fmt.Sprintf("var_%d", x))
+	}
+
+	var cursor int
+	for x := size; x > 0; x >>= 1 {
+		for y := 0; y < x-1; y += 2 {
+			n := Apply2(nodes[cursor+y], nodes[cursor+y+1], concat)
+			nodes = append(nodes, n)
+		}
+		cursor += x
+	}
+
+	gs := nodes[len(nodes)-1]
+	Initialize(testContext(), gs)
+	for n := 0; n < b.N; n++ {
+		_ = ParallelStabilize(testContext(), gs)
+		for x := 0; x < size>>2; x++ {
+			SetStale(nodes[rand.Intn(size)])
+		}
+		_ = ParallelStabilize(testContext(), gs)
+		for x := 0; x < size>>2; x++ {
+			SetStale(nodes[rand.Intn(size)])
+		}
+		_ = ParallelStabilize(testContext(), gs)
 	}
 }
 
