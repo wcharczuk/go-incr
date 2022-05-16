e5945389b23e1c20c4c8ecace932035f831dd233
Will Charczuk <wcharczuk@users.noreply.github.com>
2022-05-15 22:12:34 -0700

    updates

--- pg/migrate.go
+++ pg/migrate.go
@@ -10,18 +10,39 @@ func Migrate(ctx context.Context, c Conn) error {
 }
 
 var migrations = []migration{
-	{schemaLessThan("1.2022.05.12.1"), nil},
+	{schemaNotExists("1.2022.05.12.1"), createSchema},
+	{schemaVersionLessThan("1.2022.05.12.1"), nil},
 }
 
-type versionFilter func(context.Context, Conn) (bool, error)
+func createSchema(ctx context.Context, c Conn) error {
+	query := `CREATE SCHEMA incremental`
+	if _, err := c.ExecContext(ctx, query); err != nil {
+		return err
+	}
+	return nil
+}
+
+type filter func(context.Context, Conn) (bool, error)
 type action func(context.Context, Conn) error
 
 type migration struct {
-	versionFilter
+	filter
 	action
 }
 
-func schemaLessThan(schemaVersion string) versionFilter {
+func schemaNotExists(schema string) filter {
+	return func(ctx context.Context, c Conn) (bool, error) {
+		query := `SELECT 1 FROM information_schema.schemata WHERE schema_name = $1`
+		res, err := c.QueryContext(ctx, query, schema)
+		if err != nil {
+			return false, err
+		}
+		defer res.Close()
+		return !res.Next(), nil
+	}
+}
+
+func schemaVersionLessThan(schemaVersion string) filter {
 	return func(ctx context.Context, c Conn) (bool, error) {
 		currentSchemaVersion, err := readSchemaVersion(ctx, c)
 		if err != nil {
