028814533fabdd7739ca1ec3b4d56dc0c64339da
Will Charczuk <wcharczuk@users.noreply.github.com>
2022-05-18 21:24:17 -0700

    test dot

--- dot_test.go
+++ dot_test.go
@@ -6,12 +6,24 @@ import (
 )
 
 func Test_Dot(t *testing.T) {
+	golden := `digraph {
+	node [label="apply2[563b29ae]"]; n1
+	node [label="var[884f9774]"]; n2
+	node [label="var[7f8a4e21]"]; n3
+	n2 -> n1;
+	n3 -> n1;
+}
+`
+
 	v0 := Var("foo")
-	v0.Node().id = Identifier{}
+	v0.Node().id, _ = ParseIdentifier("165382c219e24e3db77fd41a884f9774")
 	v1 := Var("bar")
+	v1.Node().id, _ = ParseIdentifier("a985936bed8c48b99801a5bd7f8a4e21")
 	m0 := Apply2(v0.Read(), v1.Read(), concat)
+	m0.Node().id, _ = ParseIdentifier("fc45f4a7b5c7456f852f2298563b29ae")
 
 	buf := new(bytes.Buffer)
 	err := Dot(buf, m0)
 	ItsNil(t, err)
+	ItsEqual(t, golden, buf.String())
 }
