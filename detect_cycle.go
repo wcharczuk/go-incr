bd07d946dd0208375eb4915c64d9a4fc50b9689b
Will Charczuk <wcharczuk@users.noreply.github.com>
2023-10-28 11:20:56 -0700

    faster / less memory cycle detection

--- detect_cycle.go
+++ detect_cycle.go
@@ -9,29 +9,62 @@ import "fmt"
 // in special cases; the vast majority of direct use cases
 // for the incremental library cannot create graph cycles.
 func DetectCycle(child, parent INode) error {
-	getChildren := func(n INode) []INode {
-		if n.Node().id == parent.Node().id {
-			return append(parent.Node().Children(), child)
+	getParents := func(n INode) []INode {
+		if n.Node().ID() == child.Node().ID() {
+			return append(child.Node().Parents(), parent)
 		}
-		return n.Node().Children()
+		return n.Node().Parents()
 	}
-	seen := make(set[Identifier])
-	if err := cycleSeen(child, getChildren, seen); err != nil {
-		return fmt.Errorf("%w; adding %v as a child of %v would cause a cycle", err, child, parent)
+	if detectCycleFast(child.Node().ID(), parent, getParents) {
+		return fmt.Errorf("adding %v as child of %v would cause a cycle", child, parent)
 	}
 	return nil
 }
 
-func cycleSeen(n INode, getChildren func(INode) []INode, seen set[Identifier]) error {
-	if seen.has(n.Node().id) {
-		return fmt.Errorf("cycle detected at %v", n)
+func detectCycleFast(childID Identifier, startAt INode, getParents func(INode) []INode) bool {
+	if startAt.Node().ID() == childID {
+		return true
 	}
-	seen.add(n.Node().id)
-	children := getChildren(n)
-	for _, c := range children {
-		if err := cycleSeen(c, getChildren, seen.copy()); err != nil {
-			return err
+	for _, p := range getParents(startAt) {
+		if detectCycleFast(childID, p, getParents) {
+			return true
 		}
 	}
-	return nil
+	return false
 }
+
+// NOTE(wc): this is a more comprehensive, but memory intensive, cycle
+// detection method that uses a "seen" identifier set and walks up the graph
+// from the parent as if the child had been added as a child of the parent
+// through an injected `GetChildren` method.
+//
+// In practice, according to the test suite, it's not more correct
+// than the fast method but I'm keeping it here in case we need to revert.
+
+// func detectCycleSlow(child, parent INode) error {
+// 	getChildren := func(n INode) []INode {
+// 		if n.Node().id == parent.Node().id {
+// 			return append(parent.Node().Children(), child)
+// 		}
+// 		return n.Node().Children()
+// 	}
+// 	seen := make(set[Identifier])
+// 	if err := cycleSeen(child, getChildren, seen); err != nil {
+// 		return fmt.Errorf("%w; adding %v as a child of %v would cause a cycle", err, child, parent)
+// 	}
+// 	return nil
+// }
+
+// func cycleSeen(n INode, getChildren func(INode) []INode, seen set[Identifier]) error {
+// 	if seen.has(n.Node().id) {
+// 		return fmt.Errorf("cycle detected at %v", n)
+// 	}
+// 	seen.add(n.Node().id)
+// 	children := getChildren(n)
+// 	for _, c := range children {
+// 		if err := cycleSeen(c, getChildren, seen.copy()); err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
--- detect_cycle_test.go
+++ detect_cycle_test.go
@@ -31,6 +31,99 @@ func Test_DetectCycle(t *testing.T) {
 	testutil.ItsNotNil(t, err)
 }
 
+func detectCycleNode(label string) MapNIncr[any, any] {
+	n := MapN[any, any](identMany)
+	n.Node().SetLabel(label)
+	return n
+}
+
+func Test_DetectCycle_complex(t *testing.T) {
+	n0 := detectCycleNode("n0")
+	n1 := detectCycleNode("n1")
+	n2 := detectCycleNode("n2")
+
+	n1.AddInput(n0)
+	n2.AddInput(n1)
+
+	n01 := detectCycleNode("n01")
+	n02 := detectCycleNode("n02")
+
+	n01.AddInput(n2)
+	n02.AddInput(n01)
+
+	n11 := detectCycleNode("n11")
+	n12 := detectCycleNode("n12")
+	n13 := detectCycleNode("n13")
+
+	n11.AddInput(n01)
+	n12.AddInput(n11)
+	n13.AddInput(n12)
+
+	n21 := detectCycleNode("n21")
+	n22 := detectCycleNode("n22")
+	n23 := detectCycleNode("n23")
+	n24 := detectCycleNode("n24")
+
+	n21.AddInput(n11)
+	n22.AddInput(n21)
+	n23.AddInput(n22)
+	n24.AddInput(n23)
+
+	var err error
+	err = DetectCycle(n2, n02)
+	testutil.ItsNotNil(t, err)
+
+	err = DetectCycle(n2, n13)
+	testutil.ItsNotNil(t, err)
+
+	err = DetectCycle(n2, n24)
+	testutil.ItsNotNil(t, err)
+
+	err = DetectCycle(n02, n13)
+	testutil.ItsNil(t, err, "this should _not_ cause a cycle")
+
+	err = DetectCycle(n01, n13)
+	testutil.ItsNotNil(t, err)
+}
+
+func Test_DetectCycle_complex2(t *testing.T) {
+	n0 := detectCycleNode("n0")
+	n10 := detectCycleNode("n10")
+	n11 := detectCycleNode("n11")
+	n12 := detectCycleNode("n12")
+	n21 := detectCycleNode("n21")
+	n22 := detectCycleNode("n22")
+
+	n10.AddInput(n0)
+	n11.AddInput(n10)
+	n12.AddInput(n11)
+	n21.AddInput(n11)
+	n22.AddInput(n21)
+
+	err := DetectCycle(n10, n22)
+	testutil.ItsNotNil(t, err)
+}
+
+func Test_DetectCycle_2(t *testing.T) {
+	/* these are some trivial cases to make sure bases are covered */
+
+	n0 := MapN[any, any](identMany)
+	n1 := MapN[any, any](identMany)
+	n2 := MapN[any, any](identMany)
+
+	err := DetectCycle(n0, n0)
+	testutil.ItsNotNil(t, err)
+	n1.AddInput(n0)
+
+	err = DetectCycle(n2, n1)
+	testutil.ItsNil(t, err)
+
+	n2.AddInput(n1)
+
+	err = DetectCycle(n0, n2)
+	testutil.ItsNotNil(t, err)
+}
+
 func Test_DetectCycle_regression(t *testing.T) {
 	table := Var("table")
 	columnDownload := Map(table, ident)
@@ -42,7 +135,7 @@ func Test_DetectCycle_regression(t *testing.T) {
 	uploadRemaining := MapN(identMany, lastUpload)
 
 	err := DetectCycle(uploadRemaining, targetUpload)
-	testutil.ItsNil(t, err)
+	testutil.ItsNil(t, err, "this should _not_ cause a cycle!")
 }
 
 func Test_DetectCycle_regression2(t *testing.T) {
@@ -56,5 +149,5 @@ func Test_DetectCycle_regression2(t *testing.T) {
 	_ = MapN(identMany, lastUpload)
 
 	err := DetectCycle(columnDownload, table)
-	testutil.ItsNil(t, err)
+	testutil.ItsNil(t, err, "this should _not_ cause a cycle!")
 }
