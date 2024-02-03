d0cd9c7a500f1f1e3879a92fa7b8bd5baf99d08e
Will Charczuk <wcharczuk@users.noreply.github.com>
2024-02-02 17:14:45 -0800

    updates

--- link.go
+++ link.go
@@ -8,19 +8,6 @@ package incr
 // The reverse of this is `Unlink` on the child node, which
 // removes the inputs as "parents" of the "child" node.
 func Link(child INode, inputs ...INode) {
-	for _, p := range inputs {
-		if err := DetectCycleIfLinked(child, p); err != nil {
-			if child.Node().graph != nil {
-				child.Node().graph.maybeAddObservedNode(p)
-			} else {
-				p.Node().graph.maybeAddObservedNode(child)
-			}
-			child.Node().addParents(p)
-			p.Node().addChildren(child)
-			_ = dumpDot(child.Node().graph, homedir("bind_if_cycle.png"))
-			panic(err)
-		}
-	}
 	child.Node().addParents(inputs...)
 	for _, input := range inputs {
 		input.Node().addChildren(child)
