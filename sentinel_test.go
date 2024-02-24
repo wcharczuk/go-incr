2012a40f585892318bf9dd070e3ad6ad2cbe2265
Will Charczuk <wcharczuk@users.noreply.github.com>
2024-02-24 11:16:08 -0800

    adds test for bind, will want to do a nested test next

--- sentinel_test.go
+++ sentinel_test.go
@@ -78,3 +78,53 @@ func Test_Sentinel_Unwatch(t *testing.T) {
 
 	testutil.Equal(t, 2, updates)
 }
+
+func Test_Sentinel_withinBind(t *testing.T) {
+	ctx := testContext()
+	g := New()
+
+	v := Var(g, "foo")
+	var updates int
+	m := Map(g, v, func(vv string) string {
+		updates++
+		return vv + fmt.Sprintf("-mapped-%d", updates)
+	})
+	_ = Sentinel(g, func() bool {
+		return updates < 3
+	}, m)
+
+	bv := Var(g, "a")
+	b := Bind(g, bv, func(bs Scope, which string) Incr[string] {
+		if which == "a" {
+			return m
+		}
+		return Return(bs, "nope")
+	})
+	ob := MustObserve(g, b)
+
+	err := g.Stabilize(ctx)
+	testutil.NoError(t, err)
+	testutil.Equal(t, "foo-mapped-1", ob.Value())
+
+	err = g.Stabilize(ctx)
+	testutil.NoError(t, err)
+	testutil.Equal(t, "foo-mapped-2", ob.Value())
+
+	bv.Set("b")
+
+	err = g.Stabilize(ctx)
+	testutil.NoError(t, err)
+	testutil.Equal(t, "nope", ob.Value())
+
+	bv.Set("a")
+
+	err = g.Stabilize(ctx)
+	testutil.NoError(t, err)
+	testutil.Equal(t, "foo-mapped-3", ob.Value())
+
+	err = g.Stabilize(ctx)
+	testutil.NoError(t, err)
+	testutil.Equal(t, "foo-mapped-3", ob.Value())
+
+	testutil.Equal(t, 3, updates)
+}
