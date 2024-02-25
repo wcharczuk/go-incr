5ce4adf64b2b2247f5065513217789849befda63
Will Charczuk <wcharczuk@users.noreply.github.com>
2024-02-24 19:38:08 -0800

    allocations down

--- parallel_batch.go
+++ parallel_batch.go
@@ -8,25 +8,22 @@ import (
 
 func parallelBatch[A any](ctx context.Context, fn func(context.Context, A) error, work ...A) (err error) {
 	var errOnce sync.Once
-	sem := make(chan struct{}, runtime.NumCPU())
+	sem := make(chan A, runtime.NumCPU())
 	wg := new(sync.WaitGroup)
 
-	process := func(i int) {
-		defer func() {
-			<-sem
-			wg.Done()
-		}()
-		workErr := fn(ctx, work[i])
+	process := func() {
+		defer wg.Done()
+		workErr := fn(ctx, <-sem)
 		if workErr != nil {
 			errOnce.Do(func() {
 				err = workErr
 			})
 		}
 	}
-	for index := range work {
-		sem <- struct{}{}
+	for _, w := range work {
+		sem <- w
 		wg.Add(1)
-		go process(index)
+		go process()
 	}
 	wg.Wait()
 	return
