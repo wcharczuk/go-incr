3a200e170c1e7180d64ab6f16ac599f845d708f2
Will Charczuk <wcharczuk@users.noreply.github.com>
2024-02-24 19:33:55 -0800

    very slim parallel batch

--- parallel_batch.go
+++ parallel_batch.go
@@ -3,76 +3,31 @@ package incr
 import (
 	"context"
 	"runtime"
+	"sync"
 )
 
-func parallelBatch[A any](ctx context.Context, fn func(context.Context, A) error, work ...A) error {
-	workers := make([]*parallelBatchWorker[A], runtime.NumCPU())
-	readyWorkers := make(chan *parallelBatchWorker[A], runtime.NumCPU())
-	errors := make(chan error, len(work))
+func parallelBatch[A any](ctx context.Context, fn func(context.Context, A) error, work ...A) (err error) {
+	var errOnce sync.Once
+	sem := make(chan struct{}, runtime.NumCPU())
+	wg := new(sync.WaitGroup)
 
-	defer func() {
-		close(readyWorkers)
-		close(errors)
-	}()
-	for x := 0; x < runtime.NumCPU(); x++ {
-		w := &parallelBatchWorker[A]{
-			action:  fn,
-			work:    make(chan A),
-			errors:  errors,
-			stop:    make(chan struct{}),
-			stopped: make(chan struct{}),
-			done: func(w *parallelBatchWorker[A]) {
-				readyWorkers <- w
-			},
-		}
-		go w.run(ctx)
-		workers[x] = w
-		readyWorkers <- w
-	}
-	var worker *parallelBatchWorker[A]
-	for _, w := range work {
-		worker = <-readyWorkers
-		worker.work <- w
-	}
-
-	for _, worker := range workers {
-		close(worker.stop)
-		<-worker.stopped
-	}
-	if len(errors) > 0 {
-		return <-errors
-	}
-	return nil
-}
-
-type parallelBatchWorker[A any] struct {
-	action  func(context.Context, A) error
-	work    chan A
-	errors  chan error
-	stop    chan struct{}
-	stopped chan struct{}
-	done    func(*parallelBatchWorker[A])
-}
-
-func (pbw *parallelBatchWorker[A]) run(ctx context.Context) {
-	var wi A
-	var err error
-	for {
-		select {
-		case <-pbw.stop:
-			close(pbw.stopped)
-			return
-		default:
-		}
-		select {
-		case <-pbw.stop:
-			close(pbw.stopped)
-			return
-		case wi = <-pbw.work:
-			if err = pbw.action(ctx, wi); err != nil {
-				pbw.errors <- err
-			}
-			pbw.done(pbw)
+	process := func(i int) {
+		defer func() {
+			<-sem
+			wg.Done()
+		}()
+		workErr := fn(ctx, work[i])
+		if workErr != nil {
+			errOnce.Do(func() {
+				err = workErr
+			})
 		}
 	}
+	for index := range work {
+		sem <- struct{}{}
+		wg.Add(1)
+		go process(index)
+	}
+	wg.Wait()
+	return
 }
