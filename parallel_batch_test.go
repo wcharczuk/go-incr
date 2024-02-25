3ae6d0607d93653b0cffb87354c3f4db703b3b23
Will Charczuk <wcharczuk@users.noreply.github.com>
2024-02-25 03:39:48 -0800

    test tweak

--- parallel_batch_test.go
+++ parallel_batch_test.go
@@ -16,16 +16,22 @@ func Test_parallelBatch(t *testing.T) {
 		work = append(work, fmt.Sprintf("work-%d", x))
 	}
 
-	var seen []string
+	seen := make(map[string]struct{})
 	var seenMu sync.Mutex
 	err := parallelBatch[string](testContext(), func(_ context.Context, v string) error {
 		seenMu.Lock()
-		seen = append(seen, v)
+		seen[v] = struct{}{}
 		seenMu.Unlock()
 		return nil
 	}, work...)
 	testutil.NoError(t, err)
 	testutil.Equal(t, len(work), len(seen))
+
+	for x := 0; x < runtime.NumCPU()<<1; x++ {
+		key := fmt.Sprintf("work-%d", x)
+		_, hasKey := seen[key]
+		testutil.Equal(t, true, hasKey)
+	}
 }
 
 func Test_parallelBatch_error(t *testing.T) {
