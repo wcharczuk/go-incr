d2887efdd921571fb03f1913121439e336c75694
Will Charczuk <wcharczuk@users.noreply.github.com>
2024-02-27 22:23:30 -0800

    updates

--- incrutil/naive/doc.go
+++ incrutil/naive/doc.go
@@ -2,5 +2,7 @@
 package naive provides basic implementations of incremental primitives to test what a "baseline" implementation of a DAG would perform like _before_ you add incremental.
 
 The goal here is to show that for _many_ use cases, incremental is overkill and just adds overhead.
+
+Obvious caveat is obvious, but you should not actually use this package and there are zero guarantees with it.
 */
 package naive
