31501bb054b9702af93107d85236af278a8f86e2
Will Charczuk <wcharczuk@users.noreply.github.com>
2024-04-08 07:49:48 -0700

    standardize

--- cutoff2.go
+++ cutoff2.go
@@ -17,7 +17,7 @@ func Cutoff2[A, B any](bs Scope, epsilon Incr[A], input Incr[B], fn Cutoff2Func[
 // The goal of the cutoff incremental is to stop recomputation at a given
 // node if the difference between the previous and latest values are not
 // significant enough to warrant a full recomputation of the children of this node.
-func Cutoff2Context[A, B any](bs Scope, epsilon Incr[A], input Incr[B], fn Cutoff2ContextFunc[A, B]) Cutoff2Incr[A, B] {
+func Cutoff2Context[A, B any](bs Scope, epsilon Incr[A], input Incr[B], fn Cutoff2ContextFunc[A, B]) Incr[B] {
 	return WithinScope(bs, &cutoff2Incr[A, B]{
 		n:  NewNode("cutoff2"),
 		fn: fn,
@@ -26,19 +26,11 @@ func Cutoff2Context[A, B any](bs Scope, epsilon Incr[A], input Incr[B], fn Cutof
 	})
 }
 
-// CutoffIncr is an incremental node that implements the ICutoff interface.
-type Cutoff2Incr[A, B any] interface {
-	Incr[B]
-	IStabilize
-	ICutoff
-}
-
 var (
-	_ Incr[string]             = (*cutoff2Incr[int, string])(nil)
-	_ Cutoff2Incr[int, string] = (*cutoff2Incr[int, string])(nil)
-	_ IStabilize               = (*cutoff2Incr[int, string])(nil)
-	_ ICutoff                  = (*cutoff2Incr[int, string])(nil)
-	_ fmt.Stringer             = (*cutoff2Incr[int, string])(nil)
+	_ Incr[string] = (*cutoff2Incr[int, string])(nil)
+	_ IStabilize   = (*cutoff2Incr[int, string])(nil)
+	_ ICutoff      = (*cutoff2Incr[int, string])(nil)
+	_ fmt.Stringer = (*cutoff2Incr[int, string])(nil)
 )
 
 // Cutoff2Func is a function that implements cutoff checking.
